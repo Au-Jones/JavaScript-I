295e855432661602ffb310b90fa0126610f21c73
Your Name <aujones95@gmail.com>
2019-07-25 11:05:12 -0400

    finished up some problems

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -1,7 +1,5 @@
 // To help us use arrays with real world problems we are going to simulate a used car dealer that has 50 cars in their inventory.
-
 // The car dealer has all of their inventory housed in the array seen below.  Scroll down past the data to find out how you can help the car dealer.
-
 let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year":2009},
 {"id":2,"car_make":"Mazda","car_model":"Miata MX-5","car_year":2001},
 {"id":3,"car_make":"Land Rover","car_model":"Defender Ice Edition","car_year":2010},
@@ -52,10 +50,7 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 {"id":48,"car_make":"Dodge","car_model":"Magnum","car_year":2008},
 {"id":49,"car_make":"Chrysler","car_model":"Sebring","car_year":1996},
 {"id":50,"car_make":"Lincoln","car_model":"Town Car","car_year":1999}];
-
-
 // Example for loop:
-
 // arr = [1,2,3,4];
 // for (let i = 0; i < arr.length; i++) {
 //     arr[i]; // 1,2,3,4
@@ -63,54 +58,58 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-let car33 = 0
+console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
+console.log("Car 33 is a" , inventory[32]["car_year"] , inventory[32]["car_make"] , inventory[32]["car_model"]  );
 
-console.log(`Car 33 is a ${inventory[32].2011} ${inventory[32].Jeep} ${inventory[32].Wrangler}` );
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = inventory[inventory.length-1];
-console.log(lastCar.Lincoln, lastCar.Town-car);
+let LastCar = 0;
+console.log();
+let lastCar = inventory.length - 1
+console.log(inventory[lastCar]["car_make"], inventory[lastCar]["car_model"]);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-for (let i = 0; i < inventory.length; i++) {
-  carModels.push(inventory[i].car_model);
+console.log();
+for (let i=0; i<inventory.length; i++){
+    carModels.push(inventory[i]["car_model"]);
+    carModels.sort();
 }
-console.log(carModels.sort());
+console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-for (let i = 0; i < inventory.length; i++) {
-  carYears.push(inventory[i].car_year);
+console.log();
+for (let i=0; i<inventory.length; i++){
+    carYears.push(inventory[i]["car_year"]);
+    carYears.sort();
 }
 console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-let oldCars =[]; 
-for (let i = 0; i < carYears.length; i++) {
-  if(carYears[i] < 2000) {
-    oldCars.push(carYears[i]);
-  }
-  else {
-    // do nothing
-  }
+let oldCars = [];
+console.log(); 
+for (let i=0; i<inventory.length; i++){
+    if (carYears[i]<2000){
+        oldCars.push(inventory[i]["car_year"]);        
+    }
 }
-console.log(oldCars.length);
+let countOldCars = oldCars.length;
+console.log(countOldCars); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi =[]; 
-for (let i = 0; i < inventory.length; i++) {
-  if (inventory[i].car_make == "BMW") {
-    BMWAndAudi.push(inventory[i]);
-  } else if(inventory[i].car_make == "Audi") {
-    BMWAndAudi.push(inventory[i]);
-  }
+let BMWandAudi = [];
+console.log();
+let BMWAndAudi =[];
+for (let i=0; i<inventory.length; i++) {
+    if ((inventory[i]["car_make"]==="Audi") || (inventory[i]["car_make"]==="BMW")) {
+        BMWAndAudi.push(inventory[i]);
+ }
 }
-console.log("This is a list of all the BMW and Audi cars on the lot: " + JSON.stringify(BMWAndAudi));
-
-             
+console.log(BMWAndAudi);
+console.log(JSON.stringify(BMWAndAudi));
 
--- assignments/objects.js
+++ assignments/objects.js
@@ -1,16 +1,13 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
-
 // ==== Challenge 1: Writing Objects ==== 
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
-
 // 1,mmelloy0@psu.edu,Mitzi,F
 // 2,kdiben1@tinypic.com,Kennan,M
 // 3,kmummery2@wikimedia.org,Keven,M
 // 4,gmartinson3@illinois.edu,Gannie,M
 // 5,adaine5@samsung.com,Antonietta,F
-
 // Example format of an intern object: 1,examples@you.edu,Example,F
-const example = {
+const example1 = {
   "id": 0,
   "name": "Example",
   "email": "examples@you.edu",
@@ -18,47 +15,98 @@ const example = {
 }
 
 // Write your intern objects here:
+// const interns = [
+//   {"id":1, "email": "mmelloy0@psu.edu","first_name":"Mitzi",gender:"F"},
+//   {"id":2, "email": "kdiben1@tinypic.com","first_name":"Kennan",gender:"M"},
+//   {"id":3, "email": "kmummery2@wikimedia.org","first_name":"Keven",gender:"M"},
+//   {"id":4, "email": "gmartinson3@illinois.edu","first_name":"Gannie",gender:"M"},
+//   {"id":5, "email": "adaine5@samsung.com","first_name":"Antoinietta",gender:"F"}
+// ];
+const example2 = {
+  "id": 0,
+  "name": "Example",
+  "email": "examples@you.edu",
+  "gender": "F"
+};
 
+// Write your intern objects here:
+const mitzi = {
+    'id': 0,
+    'name': 'Mitzi',
+    'gender': 'F',
+    'email': 'kdiben1@tinypic.com'
+};
+
+const kennan = {
+    'id': 1,
+    'name': 'Kennan',
+    'gender': 'M',
+    'email': 'mmelloy0@psu.edu',
+    'speak': function(){
+        return "Hello, my name is Kennan";
+    }
+};
+
+const keven = {
+    'id': 2,
+    'name': 'Keven',
+    'gender': 'M',
+    'email': 'kmummery2@wikimedia.org'
+};
+
+const gannie = {
+    'id': 3,
+    'name': 'gannie',
+    'gender': 'M',
+    'email': 'gmartinson3@illinois.edu'
+};
+
+const antonietta = {
+    'id': 4,
+    'name': ' Antonietta',
+    'gender': 'F',
+    'email': 'adaine5@samsung.com',
+    'multiplyNums': function (num1, num2) {console.log(num1*num2)}
+};
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
 
+console.log(mitzi.name);
 // Kennan's ID
 
+console.log(kennan.id);
 // Keven's email
 
+console.log(keven.email);
 // Gannie's name
 
+console.log(gannie.name);
 // Antonietta's Gender
 
+console.log(antonietta.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+kennan.speak()
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+antonietta.multiplyNums(3,4);
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
-
 // 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
 // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
-
 const parent = {}
-
 // Log the parent object's name
-
 // Log the child's age
-
 // Log the name and age of the grandchild
-
 // Have the parent speak
-
 // Have the child speak
-
 // Have the grandchild speak
